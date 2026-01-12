99e3db30aacec7d0d3442f076a009d7d88c7ea4d
Jeykobz <61214350+Jeykobz@users.noreply.github.com>
2026-01-12 18:13:58 +0100

    Update reset_or_install_last_stable.sh

--- reset_or_install_last_stable.sh
+++ reset_or_install_last_stable.sh
@@ -537,6 +537,7 @@ class Runner:
                "--geometry=100%x100%", "--autofit=100%",
                "--input-default-bindings=no", "--input-vo-keyboard=no", 
                "--cursor-autohide=always", "--osc=no", "--prefetch-playlist=yes",
+               "--vo=gpu", "--gpu-context=x11" if os.environ.get("DISPLAY") else "--gpu-context=drm",
                f"--log-file=/var/log/ad-runner/mpv_player.log"]
 
         if os.environ.get("DISPLAY"):
